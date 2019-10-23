d4dc39ab55f1a9fdcbf9f2a2ce2c1dbda03de2a0
bodhibrdr <julie.gumerman@gmail.com>
2019-10-23 11:08:50 -0600

    more tests

--- users/user-model.spec.js
+++ users/user-model.spec.js
@@ -1,13 +1,28 @@
 const Users = require("./user-model.js");
-const db = require("../data/dbconfig.js")
-;
+const db = require("../data/dbconfig.js");
+
+
+/***sanity check****/
 describe("gets users", () => {
     it ("should show all users", async () => {
         await Users.getUsers()
     })
-
 })
 
+/*********login and register**********/
+describe("can register a user", () => {
+    beforeEach(async() => {
+        db("users").truncate()
+    })
+    it("registers a user", async () => {
+        await Users.add({username: "Reverie", password: "dogamole"});
+        const users = await db("users");
+        let i = users.length;
+        expect("users").toHaveLength(i++)
+    })
+})
+
+/****functionality for users********* */
 describe("gets games", () => {
     it("should get games based off a user id number", async() => {
        await Users.getGames(1);
